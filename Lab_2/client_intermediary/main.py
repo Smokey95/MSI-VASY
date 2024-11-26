14f42fca8ab25c5922ef0f5d44c2abb84bf0390d
Smokey95 <97231524+Smokey95@users.noreply.github.com>
2024-11-26 22:53:59 +0100

    some test changes on intermediary code

--- Lab_2/client_intermediary/main.py
+++ Lab_2/client_intermediary/main.py
@@ -1,20 +1,28 @@
-from xbee import transmit, receive
+from xbee import transmit, receive, atcmd
 import time
 
-from network_utility import NetworkHeader
+from network_utility import Packet
 
-def debug_test_package():
-    test_flooding_package = NetworkHeader(0, "0x69", "0x420", "OF")
-    print(test_flooding_package)
 
 def main():
-    print("Hello World")
-    debug_test_package()
+    print("started intermediary client")
+
+    mac_local = atcmd("MY")
+    print("local mac address: {}".format(mac_local))
 
     while(True):
         msg = receive()
         if msg:
-            print(msg)
+            payload = Packet.from_bytearray(msg.get("payload"))
+            print("received payload {}".format(payload))
+
+            if payload.is_flooding():
+                print("flooding...")
+                if payload.get_dest_addr() == mac_local:
+                    print("i am dest of flooding")
+                elif payload.get_dest_addr() != mac_local:
+                    print("i am NOT dest of flooding")
+
 
 
 if __name__ == '__main__':
